db7c7c66f04dd710699beeae7d7d3c9b517504c2
Andrey rachitsky <>
2022-11-14 21:42:38 +0300

    initial commit

---
+++ UsefullMap/UsefullMap/UsefullMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsefullMapApp.swift
+//  UsefullMap
+//
+//  Created by Andrey Rachitskiy on 14.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct UsefullMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
